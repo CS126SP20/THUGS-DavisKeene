1e032e2bd74959bdcc8782b4db509bc25eaed0f0
Davis <dbkeene.tsyc@gmail.com>
2020-04-18 15:00:35 -0400

    added: FastNoise library to project

---
+++ include/mylibrary/Terrain.h
@@ -0,0 +1,8 @@
+//
+// Created by davis on 4/18/20.
+//
+
+#ifndef FINALPROJECT_TERRAIN_H
+#define FINALPROJECT_TERRAIN_H
+
+#endif //FINALPROJECT_TERRAIN_H
---
+++ src/Terrain.cpp
@@ -0,0 +1,6 @@
+//
+// Created by davis on 4/18/20.
+//
+
+#include "mylibrary/Terrain.h"
+
